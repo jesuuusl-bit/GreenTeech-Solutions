519157ca41b8de41c7da18c3b7489dab4f3e18ca
jesuuusl-bit <07lopezjesus11@gmail.com>
2025-11-10 22:43:38 -0500

    fix Vite API Url localhost for render Url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // ===== frontend/src/services/api.js =====
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://greentech-api-gateway.onrender.com' //'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
